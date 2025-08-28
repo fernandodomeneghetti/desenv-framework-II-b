0bafcaeb864af3f4800aad961c97956379e2ae35
Fernando Miranda Domeneghetti <fernando.miranda@anchieta.br>
2025-08-27 21:41:24 -0300

    add swagger

---
+++ doc/extend.js
@@ -0,0 +1,20 @@
+const swaggerOptions =  {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Alunos',
+            version: '1.0.0',
+            description: 'API de gerenciamento de alunos'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Servidor de desenvolvimento'
+            }
+        ]
+    },
+    apis: ['./index.js']
+}
+
+module.exports = swaggerOptions
+
--- index.js
+++ index.js
@@ -1,23 +1,125 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerOptions = require('./doc/extend')
+
 const app = express()
 const port = 3000
 
+const specs = swaggerJsdoc(swaggerOptions)
+
 app.use(express.json())
 
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Aluno:
+ *          type: object
+ *          required:
+ *              - id
+ *              - ra
+ *              - nome
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: Identificador unico do aluno
+ *              nome:
+ *                  type: string
+ *                  description: Nome do aluno
+ *              ra:
+ *                  type: integer
+ *                  description: Número da matrícula
+ *          example:
+ *              id: 1
+ *              nome: Fulano
+ *              ra: 123    
+ */
 let alunos = [
     { "id": 1, "ra": 123, "nome": "Fulano" }
 ]
 
+/**
+ * @swagger
+ * /aluno:
+ *  get:
+ *      summary: Retorna todos os alunos
+ *      tags: [Alunos]
+ *      responses:
+ *          200:
+ *              description: Lista de alunos
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Aluno'
+ */
 app.get("/aluno", (req, res) => {
     res.json(alunos)
 })
 
+/**
+ * @swagger
+ * /aluno:
+ *  post:
+ *      summary: Cadastros de aluno
+ *      tags: [Alunos]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          nome:
+ *                              type: string
+ *                          ra:
+ *                              type: integer
+ *      responses:
+ *          201:
+ *              description: Cadastro de alunos
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Aluno'
+ */
 app.post("/aluno", (req, res) => {
     const novoAluno = { id: alunos.length + 1, ...req.body }
     alunos.push(novoAluno)
     res.status(201).json(novoAluno)
 })
 
+/**
+ * @swagger
+ * /aluno:
+ *  put:
+ *      summary: Atualização de aluno
+ *      tags: [Alunos]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            schema:
+ *              type: integer
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          nome:
+ *                              type: string
+ *                          ra:
+ *                              type: integer
+ *      responses:
+ *          200:
+ *              description: Aluno atualizado
+ *          404:
+ *              description: Cara tem certeza que é esse id?               
+ */
 app.put("/aluno/:id", (req, res) => {
     const {id} = req.params
     const alunoIndex = alunos.findIndex(a => a.id == id)
@@ -29,6 +131,8 @@ app.put("/aluno/:id", (req, res) => {
         res.status(404).json({"message": "Cara tem certeza que é esse id?"})
     }
 })
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.listen(port, () => {
     console.log("Servidor de API rodando")
